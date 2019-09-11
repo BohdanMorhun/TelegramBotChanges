0ce86dd8ea226391dab11de68d604e6c426c250e
BohdanMorhun <morgunbo@ukr.net>
2019-09-11 09:50:00 +0300

    started creating bot

---
+++ src/main/java/ChatBot/Bot.java
@@ -0,0 +1,44 @@
+package ChatBot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botApi = new TelegramBotsApi();
+
+        try {
+            botApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+
+
+    @Override
+    public String getBotUsername() {
+        return "USER";
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
